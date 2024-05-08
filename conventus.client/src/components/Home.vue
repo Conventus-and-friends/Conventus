dbff5ee2b0486da67a3faef2c8185db8d130b240
zlElo <mail@zlelo.de>
2024-05-08 15:39:53 +0200

    build project base

---
+++ conventus.client/src/components/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+    <div>
+        Welcome to Conventus!
+    </div>
+</template>
