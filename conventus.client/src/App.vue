1570855f1cbbe4fb4e48bcdc3351220bcea624e2
zlElo <mail@zlelo.de>
2024-05-08 15:46:16 +0200

    fix weird stuff

--- conventus.client/src/App.vue
+++ conventus.client/src/App.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
-<<<<<<< HEAD
 import Home from './components/Home.vue'
-=======
-    import HelloWorld from './components/HelloWorld.vue'
-    import TheWelcome from './components/TheWelcome.vue'
->>>>>>> 834b96c9f55dc4675bb26d9fcaf1dcc5b8674191
 </script>
 
 <template>
     <header>
-<<<<<<< HEAD
         <img alt="Logo" class="logo" src="./assets/Conventus.svg" width="125" height="125" />
 
         <div class="wrapper">
@@ -45,45 +39,4 @@ header {
         flex-wrap: wrap;
     }
 }
-=======
-        <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-        <div class="wrapper">
-            <HelloWorld msg="You did it!" />
-        </div>
-    </header>
-
-    <main>
-        <TheWelcome />
-    </main>
-</template>
-
-<style scoped>
-    header {
-        line-height: 1.5;
-    }
-
-    .logo {
-        display: block;
-        margin: 0 auto 2rem;
-    }
-
-    @media (min-width: 1024px) {
-        header {
-            display: flex;
-            place-items: center;
-            padding-right: calc(var(--section-gap) / 2);
-        }
-
-        .logo {
-            margin: 0 2rem 0 0;
-        }
-
-        header .wrapper {
-            display: flex;
-            place-items: flex-start;
-            flex-wrap: wrap;
-        }
-    }
->>>>>>> 834b96c9f55dc4675bb26d9fcaf1dcc5b8674191
 </style>
