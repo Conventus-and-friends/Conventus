cdbe6b877c54a47a4bbc248a365556d633d98c91
Juff-Ma <contact@juffma.de>
2024-05-11 19:19:30 +0200

    Rename posts table TODO: write mapper

--- Conventus.Server/Migrations/20240511171209_RenameTablePost.Designer.cs
+++ Conventus.Server/Migrations/20240511171209_RenameTablePost.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Conventus.Server;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Conventus.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240511171209_RenameTablePost")]
+    partial class RenameTablePost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
---
+++ Conventus.Server/Migrations/20240511171209_RenameTablePost.cs
@@ -0,0 +1,78 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Conventus.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTablePost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_Categories_CategoryId",
+                table: "Post");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Post",
+                table: "Post");
+
+            migrationBuilder.RenameTable(
+                name: "Post",
+                newName: "Posts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Post_CategoryId",
+                table: "Posts",
+                newName: "IX_Posts_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Categories_CategoryId",
+                table: "Posts",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Categories_CategoryId",
+                table: "Posts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts");
+
+            migrationBuilder.RenameTable(
+                name: "Posts",
+                newName: "Post");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Posts_CategoryId",
+                table: "Post",
+                newName: "IX_Post_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Post",
+                table: "Post",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_Categories_CategoryId",
+                table: "Post",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
