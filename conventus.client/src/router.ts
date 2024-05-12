93fd3ee67cf0521beb7f02804cdaff24516086a5
zlElo <mail@zlelo.de>
2024-05-12 18:41:34 +0200

    implement basic version of post view

--- conventus.client/src/router.ts
+++ conventus.client/src/router.ts
@@ -1,6 +1,7 @@
 import HomeView from '@/components/views/HomeView.vue'
 import CategoryView from '@/components/views/CategoryView.vue'
 import E404View from '@/components/views/errors/E404View.vue'
+import PostView from '@/components/views/PostView.vue'
 import { createRouter, createMemoryHistory, createWebHistory } from 'vue-router'
 import { getLocale, loadLocaleMessages, setI18nLanguage, SUPPORT_LOCALES } from './i18n'
 import type { I18n } from 'vue-i18n'
@@ -11,6 +12,7 @@ export function setupRouter(i18n: I18n) {
     const views = [
         { path: '/:locale/', component: HomeView, name: 'home' },
         { path: '/:locale/:category/', component: CategoryView, name: 'category' },
+        { path: '/:locale/:category/:post/', component: PostView, name: 'post' },
         { path: '/:locale/error/404/', component: E404View, name: '404' },
         { path: '/', redirect: () => `/${locale}` },
         { path: '/:pathMatch(.*)*', redirect: () => `/${locale}/error/404` }
