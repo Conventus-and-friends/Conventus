3c7508173508e058d90232ebfd16608d4a0d5be8
zlElo <mail@zlelo.de>
2024-05-12 13:25:58 +0200

    fixed bug of double named css classes

--- conventus.client/src/components/views/HomeView.vue
+++ conventus.client/src/components/views/HomeView.vue
@@ -18,11 +18,11 @@ const { t } = useI18n()
     </div>
 
     <div class="flex-container">
-        <Panel class="flex-item last-item" :header="t('startpage.categories')">
+        <Panel class="flex-item-normal last-item" :header="t('startpage.categories')">
             <Categories/>
         </Panel>
 
-        <Panel class="flex-item" :header="t('startpage.relevant')">
+        <Panel class="flex-item-normal" :header="t('startpage.relevant')">
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
