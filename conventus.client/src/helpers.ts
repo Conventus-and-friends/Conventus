a86bd8412292e579dd220355695f2eeeefa36cbb
Juff-Ma <contact@juffma.de>
2024-05-14 19:44:43 +0200

    add date helpers and update model

--- conventus.client/src/helpers.ts
+++ conventus.client/src/helpers.ts
@@ -26,3 +26,12 @@ const htmlEntitiesLookup = {
 export function removeHtmlEntities(text: string): string {
     return text.replace(htmlEntitiesRegex, (match) => htmlEntitiesLookup[match as keyof typeof htmlEntitiesLookup])
 }
+
+export function dateAsUtcDate(date: Date): Date {
+    return new Date(Date.UTC(date.getFullYear(), 
+                                date.getMonth(), 
+                                date.getDate(), 
+                                date.getHours(), 
+                                date.getMinutes(), 
+                                date.getSeconds()));
+}
--- conventus.client/src/models/post.ts
+++ conventus.client/src/models/post.ts
@@ -2,5 +2,6 @@ export interface Post {
     id: string | undefined,
     title: string,
     content: string | undefined,
-    category: number
+    category: number,
+    created: Date | undefined
 }
