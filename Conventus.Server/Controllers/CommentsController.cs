49c80eb61a7ba6dfb6437332e0e9f281cf887f59
Juff-Ma <contact@juffma.de>
2024-05-15 17:23:58 +0200

    Initial comment controller

--- Conventus.Server/Controllers/CommentsController.cs
+++ Conventus.Server/Controllers/CommentsController.cs
@@ -1,23 +1,26 @@
 
-using Conventus.Server;
 using Conventus.Server.Models;
 using Conventus.Server.Models.DTO;
 using Conventus.Server.Models.Mappers;
 using Ganss.Xss;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Conventus.Server.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public sealed class CommentsController(
     ApplicationDbContext context,
     HtmlSanitizer sanitizer,
-    ILogger<CommentsController> logger) 
-    : ControllerBase 
+    ILogger<CommentsController> logger)
+    : ControllerBase
 {
     private readonly ApplicationDbContext _dbContext = context;
     private readonly HtmlSanitizer _htmlSanitizer = sanitizer;
     private readonly ILogger<CommentsController> _logger = logger;
 
+    [HttpGet]
     public ActionResult<IEnumerable<CommentDto>> Get([FromQuery] Pager pager)
     {
         if (!pager.IsValid())
@@ -28,5 +31,76 @@ public sealed class CommentsController(
         return Ok(_dbContext.Comments
                 .Skip((pager.Page - 1) * pager.Length).Take(pager.Length)
                 .Select(x => x.ToDto()));
+    }
+
+    [HttpGet("by-id/{id}")]
+    public async Task<ActionResult<CommentDto>> Get(Guid id)
+    {
+        var comment = await _dbContext.Comments.FindAsync(id);
+        if (comment is null)
+        {
+            return NotFound();
+        }
+        return Ok(comment.ToDto());
+    }
+
+    [HttpGet("by-post/{postId}")]
+    public ActionResult<IEnumerable<CommentDto>> Get(Guid postId, [FromQuery] Pager pager)
+    {
+        if (!pager.IsValid())
+        {
+            return BadRequest();
+        }
+
+        var comments = _dbContext.Comments
+            .Where(x => x.PostId == postId)
+            .Skip((pager.Page - 1) * pager.Length).Take(pager.Length);
+
+        return Ok(comments.Select(x => x.ToDto()));
+    }
+
+    [HttpGet("count")]
+    public async Task<int> GetCount()
+    {
+        return await _dbContext.Comments.CountAsync();
+    }
+
+    [HttpGet("by-post/{postId}/count")]
+    public async Task<int> GetCount(Guid postId)
+    {
+        var comments = _dbContext.Comments
+            .Where(x => x.PostId == postId);
+
+        return await comments.CountAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CommentDto>> Post([FromBody] CommentDto comment)
+    {
+        if (!comment.IsValid())
+        {
+            return BadRequest();
+        }
+
+        comment.Content = _htmlSanitizer.Sanitize(comment.Content);
+
+        comment.Created = DateTime.UtcNow;
+
+        var result = await _dbContext.Comments.AddAsync(comment.ToEntity());
+
+        try
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+        catch (DbUpdateException)
+        {
+            return BadRequest();
+        }
+
+        comment.Id = result.Entity.Id;
+
+        _logger.LogDebug("Added comment {Id} to post with id: {PostId}", comment.Id, comment.PostId);
+
+        return Ok(comment);
     }
 }
--- Conventus.Server/Models/Mappers/PostMapper.cs
+++ Conventus.Server/Models/Mappers/PostMapper.cs
@@ -10,10 +10,12 @@ public static partial class PostMapper
     [MapperIgnoreSource(nameof(Post.Category))]
     [MapperIgnoreSource(nameof(Post.TimeCreated))]
     [MapperIgnoreSource(nameof(Post.DateCreated))]
+    [MapperIgnoreSource(nameof(Post.Comments))]
     public static partial PostDto ToDto(this Post post);
 
     [MapperIgnoreTarget(nameof(Post.Category))]
     [MapperIgnoreTarget(nameof(Post.Created))]
+    [MapperIgnoreTarget(nameof(Post.Comments))]
     [MapProperty(nameof(PostDto.Created), nameof(Post.DateCreated))]
     [MapProperty(nameof(PostDto.Created), nameof(Post.TimeCreated))]
     public static partial Post ToEntity(this PostDto postDto);
